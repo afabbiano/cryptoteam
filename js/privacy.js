4a876154f5c0e05e97ef6e64d02f8c5e0a1f4f73
Stefano Martinelli <stefano.ste.martinelli@gmail.com>
2017-09-26 10:10:34 +0200

    Spostamento javascript in file

---
+++ js/privacy.js
@@ -0,0 +1,16 @@
+/*!
+ * privacy.js v.1.0
+ * by Stefano martinelli
+ *
+ * Codice javascript per impostare i cookie nel popup privacy
+ *
+ * Copyright 2017 Stefano Martinelli
+ * Released under the MIT license
+ */
+
+ document.getElementById("pryvacyButton").addEventListener("click", impostaCookie);
+
+ function impostaCookie(){
+     $.cookie('privacy-ack-20170913', 'true', { expires: 365, path: '/{{site.lang}}' });
+     $('#privacy-message').hide();
+ };
